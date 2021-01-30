f6ff37d1ad7149ee12b0848b3818b2ec4fba98c7
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-30 19:48:33 +0000

    Score Zone Prefabs

---
+++ Lost and Found - GGJ 2021/Assets/Scripts/CollectionZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CollectionZone : Zone
+    {
+
+        [SerializeField] private BeanType zoneType;
+        [SerializeField] private DeathType deathType;
+
+        public override void onZoneEntered(GameObject gameObject)
+        {
+            if (zoneType == gameObject.GetComponent<BeanPersonType>().Type)
+            {
+                ScoreManager.addToScore();
+            }
+            else 
+            {
+                gameObject.GetComponent<BeanPersonHealth>().kill(deathType);
+            }
+        }
+
+        public override void onZoneLeft(GameObject beanPerson)
+        {
+            
+        }
+
+
+        
+    }
+}
