33a49971e58f44ab0c6357d3ccc8320e650920a8
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-30 12:24:15 +0000

    Tractor Beam Start

---
+++ Lost and Found - GGJ 2021/Assets/Scripts/PlayerTractorBeam.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTractorBeam : MonoBehaviour
+{
+    bool tractorBeamOn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (tractorBeamOn)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+}
