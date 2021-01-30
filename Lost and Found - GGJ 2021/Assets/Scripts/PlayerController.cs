73a7053bdcbcbeaf682657ae52f215c92ad191a1
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-30 12:12:56 +0000

    Spaceship movement

---
+++ Lost and Found - GGJ 2021/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMovement;
+
+    [Header("Player Input Settings")]
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float movementSmoothingSpeed = 1f;
+    private Vector3 rawInputMovement;
+    private Vector3 smoothInputMovement;
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
+    }
+
+    public void OnPickUp(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            print("Started PickUP");
+        }
+        else if(value.canceled)
+        {
+            print("Canceled PickUP");
+        }
+    }
+
+    void Update()
+    {
+        CalculateMovementInputSmoothing();
+        UpdatePlayerMovement();
+    }
+
+    private void CalculateMovementInputSmoothing()
+    {
+        smoothInputMovement = Vector3.Lerp(smoothInputMovement, rawInputMovement, Time.deltaTime * movementSmoothingSpeed);
+    }
+
+    void UpdatePlayerMovement()
+    {
+        playerMovement.updateMovementData(smoothInputMovement);
+    }
+}
---
+++ Lost and Found - GGJ 2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    [Header("Component References")]
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject model;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float movementSpeed = 3f;
+    [SerializeField] private float tiltSpeed = 10f;
+    [SerializeField] private float maxTiltAngle = 25f;
+
+
+
+
+    Vector3 movementDirection;
+
+
+
+    void FixedUpdate()
+    {
+        movePlayer();
+        tiltPlayerInMovementDirection();
+    }
+
+    private void tiltPlayerInMovementDirection()
+    {
+        if (playerRigidbody.velocity.magnitude > 0.1f)
+        {
+            model.transform.rotation = Quaternion.LookRotation(playerRigidbody.velocity, Vector3.up);
+        }
+        float tiltAngle = FloatExtensions.Map(playerRigidbody.velocity.magnitude, 0, movementSpeed, 0, maxTiltAngle);
+        /*Quaternion targetRotation = Quaternion.Euler(0 + tiltAngle
+                                                      , model.transform.rotation.eulerAngles.y
+                                                      , model.transform.rotation.eulerAngles.z);*/
+        Quaternion targetRotation = Quaternion.AngleAxis(tiltAngle, model.transform.right);
+        model.transform.rotation = Quaternion.Slerp(model.transform.rotation, targetRotation, tiltSpeed * Time.deltaTime);
+
+
+    }
+
+    private void movePlayer()
+    {
+        Vector3 movement = CameraDirection(movementDirection) * movementSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    public void updateMovementData(Vector3 smoothMovementInput)
+    {
+        movementDirection = smoothMovementInput;
+    }
+
+    Vector3 CameraDirection(Vector3 movementDirection)
+    {
+        var cameraForward = mainCamera.transform.forward;
+        var cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        return cameraForward * movementDirection.z + cameraRight * movementDirection.x;
+
+    }
+}
