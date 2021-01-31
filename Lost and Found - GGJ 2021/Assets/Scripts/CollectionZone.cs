0cd16f77297178451be00741b5749d055d5d7d6c
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-31 13:37:13 +0000

    Update CollectionZone.cs

--- Lost and Found - GGJ 2021/Assets/Scripts/CollectionZone.cs
+++ Lost and Found - GGJ 2021/Assets/Scripts/CollectionZone.cs
@@ -15,7 +15,6 @@ public class CollectionZone : Zone
         if (zoneType == gameObject.GetComponent<BeanPersonType>().Type)
         {
             gameObject.tag = "Collected";
-            EndGameManager.subtractFromNumberOfBeans();
             gameObject.GetComponent<BeanPersonMovement>().addPlaceToReach(exitPoint.position, false);
             ScoreManager.addToScore();
             BeanPersonHealth health;
